6f737c541cdaf2b02b9fe42ad198a2609608ac18
Nikolay Ivanov <Nikolay.Ivanov@ivanov.avsmedia.net>
2011-05-24 19:43:00 +0400

    development

---
+++ trunk/Jabber.Net.Server/Connections/XmppStreamParseErrorArgs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Jabber.Net.Server.Connections
+{
+    public class XmppStreamParseErrorArgs : EventArgs
+    {
+        public Guid ConnectionId
+        {
+            get;
+            private set;
+        }
+
+        public Exception Error
+        {
+            get;
+            private set;
+        }
+
+
+        public XmppStreamParseErrorArgs(Guid connectionId, Exception error)
+        {
+            ConnectionId = connectionId;
+            Error = error;
+        }
+    }
+}
---
+++ trunk/Jabber.Net.Server/Connections/XmppStreamParsedArgs.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace Jabber.Net.Server.Connections
+{
+    public class XmppStreamParsedArgs : EventArgs
+    {
+        public Guid ConnectionId
+        {
+            get;
+            private set;
+        }
+
+        public XObject Xmpp
+        {
+            get;
+            private set;
+        }
+
+
+        public XmppStreamParsedArgs(Guid connectionId, XObject xmpp)
+        {
+            ConnectionId = connectionId;
+            Xmpp = xmpp;
+        }
+    }
+}
