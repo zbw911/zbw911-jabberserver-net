761839761a29c335bed012d434322813d6ce9c81
Nikolay Ivanov <Nikolay.Ivanov@ivanov.avsmedia.net>
2011-06-17 18:16:30 +0400

    development

---
+++ trunk/Jabber.Net.Server/Tests/TmpTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Jabber.Net.Server.Tests
+{
+    [TestFixture]
+    public class TmpTest
+    {
+        [Test]
+        public void TmpMethod()
+        {
+            var e = Activator.CreateInstance(typeof(Exception), 3);
+        }
+    }
+}
