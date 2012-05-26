33408e2474136c34dba97b09ecaed675e839aaed
Nikolay Ivanov <Nikolay.Ivanov@ivanov.avsmedia.net>
2012-05-26 15:18:07 +0000

    develop janner.net

---
+++ trunk/Jabber.Net.Server/Utils/IdGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Jabber.Net.Server.Utils
+{
+    static class IdGenerator
+    {
+        private static int counter = 0;
+
+
+        public static string NewId()
+        {
+            return Interlocked.Increment(ref counter).ToString();
+        }
+    }
+}
