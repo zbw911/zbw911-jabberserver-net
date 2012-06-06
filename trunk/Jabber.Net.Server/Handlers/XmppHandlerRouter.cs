fdb32babf1e3cdb333fa5f8dee275958b875955e
Nikolay Ivanov <Nikolay.Ivanov@ivanov.avsmedia.net>
2012-06-06 16:14:02 +0000

    develop janner.net

--- trunk/Jabber.Net.Server/Handlers/XmppHandlerRouter.cs
+++ trunk/Jabber.Net.Server/Handlers/XmppHandlerRouter.cs
@@ -5,28 +5,31 @@ using System.Reflection;
 using agsXMPP;
 using agsXMPP.Xml.Dom;
 using Jabber.Net.Server.Sessions;
+using Jabber.Net.Server.Utils;
 using Jabber.Net.Xmpp;
 
 namespace Jabber.Net.Server.Handlers
 {
     class XmppHandlerRouter
     {
+        private readonly IUniqueId uniqueId = new IncrementalUniqueId();
+        private readonly Dictionary<Type, MethodInfo> registers = new Dictionary<Type, MethodInfo>();
+        private readonly Dictionary<string, List<IInvoker>> invokers = new Dictionary<string, List<IInvoker>>(50);
         private readonly MethodInfo registerMethod;
-        private readonly IDictionary<Type, MethodInfo> registers = new Dictionary<Type, MethodInfo>();
-        private readonly IDictionary<Type, IInvoker> tests = new Dictionary<Type, IInvoker>();
 
 
         public XmppHandlerRouter()
         {
-            registerMethod = GetType().GetMethod("RegisterHandlerGeneric");
+            registerMethod = GetType().GetMethod("RegisterHandlerInternal", BindingFlags.Instance | BindingFlags.NonPublic);
         }
 
 
-        public void RegisterHandler(Jid jid, object handler)
+        public string RegisterHandler(Jid jid, object handler)
         {
             Args.NotNull(jid, "jid");
             Args.NotNull(handler, "handler");
 
+            var id = string.Empty;
             foreach (var m in handler.GetType().GetMethods().Where(m => m.Name == "ProcessElement"))
             {
                 var parameters = m.GetParameters();
@@ -45,31 +48,93 @@ namespace Jabber.Net.Server.Handlers
                         }
                         register = registers[elementType];
                     }
-                    register.Invoke(this, new object[] { jid, Delegate.CreateDelegate(register.GetParameters()[1].ParameterType, handler, m) });
+                    if (string.IsNullOrEmpty(id))
+                    {
+                        id = uniqueId.CreateId();
+                    }
+                    register.Invoke(this, new object[] { jid, Delegate.CreateDelegate(register.GetParameters()[1].ParameterType, handler, m), id });
                 }
             }
+
+            if (string.IsNullOrEmpty(id))
+            {
+                throw new ArgumentException("Xmpp element handler not found.");
+            }
+
+            return id;
         }
 
-        public void RegisterHandlerGeneric<T>(Jid jid, Func<T, XmppSession, XmppHandlerContext, XmppHandlerResult> handler) where T : Element
+        public string RegisterHandler<T>(Jid jid, Func<T, XmppSession, XmppHandlerContext, XmppHandlerResult> handler) where T : Element
         {
             Args.NotNull(jid, "jid");
             Args.NotNull(handler, "handler");
 
-            tests[typeof(T)] = new Invoker<T>(handler);
+            var id = uniqueId.CreateId();
+            RegisterHandlerInternal<T>(jid, handler, id);
+            return id;
         }
-        
+
+        public void UnregisterHandler(string id)
+        {
+            if (!string.IsNullOrEmpty(id))
+            {
+                lock (invokers)
+                {
+                    foreach (var list in invokers.Values)
+                    {
+                        list.RemoveAll(i => i.Id == id);
+                    }
+                    foreach (var pair in new Dictionary<string, List<IInvoker>>(invokers))
+                    {
+                        if (pair.Value.Count == 0)
+                        {
+                            invokers.Remove(pair.Key);
+                        }
+                    }
+                }
+            }
+        }
 
         public IEnumerable<IInvoker> GetHandlers(XmppElement e, Jid j)
         {
             Args.NotNull(j, "j");
             Args.NotNull(e, "e");
 
-            return new[] { tests[e.Element.GetType()] };
+            var key = GetKey(e.Element.GetType(), j);
+            lock (invokers)
+            {
+                List<IInvoker> list;
+                return invokers.TryGetValue(key, out list) ? list.ToArray() : new IInvoker[0];
+            }
+        }
+
+
+        private void RegisterHandlerInternal<T>(Jid jid, Func<T, XmppSession, XmppHandlerContext, XmppHandlerResult> handler, string id) where T : Element
+        {
+            Args.NotNull(jid, "jid");
+            Args.NotNull(handler, "handler");
+
+            var key = GetKey(typeof(T), jid);
+            lock (invokers)
+            {
+                if (!invokers.ContainsKey(key))
+                {
+                    invokers[key] = new List<IInvoker>();
+                }
+                invokers[key].Add(new Invoker<T>(handler, id));
+            }
+        }
+
+        private string GetKey(Type type, Jid jid)
+        {
+            return type.FullName + jid.ToString();
         }
 
 
         public interface IInvoker
         {
+            string Id { get; }
+
             XmppHandlerResult ProcessElement(XmppElement e, XmppSession s, XmppHandlerContext c);
         }
 
@@ -77,10 +142,13 @@ namespace Jabber.Net.Server.Handlers
         {
             private readonly Func<T, XmppSession, XmppHandlerContext, XmppHandlerResult> handler;
 
+            public string Id { get; private set; }
 
-            public Invoker(Func<T, XmppSession, XmppHandlerContext, XmppHandlerResult> handler)
+
+            public Invoker(Func<T, XmppSession, XmppHandlerContext, XmppHandlerResult> handler, string id)
             {
                 this.handler = handler;
+                Id = id;
             }
 
             public XmppHandlerResult ProcessElement(XmppElement e, XmppSession s, XmppHandlerContext c)
