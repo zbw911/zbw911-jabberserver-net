680d91db5da710bfb985927cdb7a9c25e82f3ec6
Nikolay Ivanov <Nikolay.Ivanov@ivanov.avsmedia.net>
2012-06-08 08:54:37 +0000

    develop janner.net

--- trunk/Jabber.Net.Server/Handlers/XmppSendResult.cs
+++ trunk/Jabber.Net.Server/Handlers/XmppSendResult.cs
@@ -7,15 +7,17 @@ namespace Jabber.Net.Server.Handlers
     public class XmppSendResult : XmppHandlerResult
     {
         private readonly XmppSession session;
+        private readonly bool offline;
         private readonly Element[] elements;
 
-        public XmppSendResult(XmppSession session, params Element[] elements)
+        public XmppSendResult(XmppSession session, bool offline, params Element[] elements)
         {
             Args.NotNull(session, "session");
             Args.NotNull(elements, "elements");
             Args.Requires<ArgumentOutOfRangeException>(0 < elements.Length, "Elements list is empty.");
 
             this.session = session;
+            this.offline = offline;
             this.elements = elements;
         }
 
@@ -25,8 +27,13 @@ namespace Jabber.Net.Server.Handlers
 
             foreach (var e in elements)
             {
-                session.EndPoint.Send(e, null);
+                session.EndPoint.Send(e, offline ? notsended => Save(notsended, context) : (Action<Element>)null);
             }
+        }
+
+        private void Save(Element e, XmppHandlerContext context)
+        {
+            //context.Storage<IOfflineStorage>().Save(e);
         }
     }
 }
