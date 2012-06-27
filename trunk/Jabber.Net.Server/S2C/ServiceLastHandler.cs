ecdc9645a9317467ba13202bfe03ee2443fc8a4b
Nikolay Ivanov <Nikolay.Ivanov@ivanov.avsmedia.net>
2012-06-27 14:05:40 +0000

    develop janner.net

---
+++ trunk/Jabber.Net.Server/S2C/ServiceLastHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using agsXMPP.protocol.client;
+using agsXMPP.protocol.iq.last;
+using Jabber.Net.Server.Handlers;
+using Jabber.Net.Server.Sessions;
+
+namespace Jabber.Net.Server.S2C
+{
+    class ServiceLastHandler : XmppHandler, IXmppHandler<LastIq>
+    {
+        private readonly DateTime started = DateTime.UtcNow;
+
+
+        [IQType(IqType.get)]
+        public XmppHandlerResult ProcessElement(LastIq element, XmppSession session, XmppHandlerContext context)
+        {
+            element.SwitchDirection();
+            element.Type = IqType.result;
+            element.Query.Seconds = (int)(DateTime.UtcNow - started).TotalSeconds;
+            return Send(session, element);
+        }
+    }
+}
