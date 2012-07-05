e2578880a65cdddd6a7ce2506305173c34978620
Nikolay Ivanov <Nikolay.Ivanov@ivanov.avsmedia.net>
2012-07-05 16:39:54 +0000

    develop janner.net

--- trunk/Jabber.Net.Server/Connections/IXmppEndPoint.cs
+++ trunk/Jabber.Net.Server/Connections/IXmppEndPoint.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 using agsXMPP.Xml.Dom;
 
 namespace Jabber.Net.Server.Connections
@@ -11,11 +12,15 @@ namespace Jabber.Net.Server.Connections
             set;
         }
 
+        bool SupportTls { get; }
+
 
         void Send(Element e, Action<Element> error);
 
         void Close();
 
         void Reset();
+
+        void StartTls(X509Certificate certificate);
     }
 }
