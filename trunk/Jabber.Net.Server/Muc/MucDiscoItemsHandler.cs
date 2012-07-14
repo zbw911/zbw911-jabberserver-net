530e4fdd39c66f8280494c11fc56a1a5a72dbca7
Nikolay Ivanov <Nikolay.Ivanov@ivanov.avsmedia.net>
2012-07-14 14:27:24 +0000

    develop janner.net

---
+++ trunk/Jabber.Net.Server/Muc/MucDiscoItemsHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using agsXMPP.protocol.iq.disco;
+using Jabber.Net.Server.Handlers;
+using Jabber.Net.Server.Sessions;
+using Jabber.Net.Server.Storages;
+
+namespace Jabber.Net.Server.MUC
+{
+    class MucDiscoItemsHandler : XmppHandler,
+        IXmppHandler<DiscoItems>
+    {
+        public XmppHandlerResult ProcessElement(DiscoItems element, XmppSession session, XmppHandlerContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
