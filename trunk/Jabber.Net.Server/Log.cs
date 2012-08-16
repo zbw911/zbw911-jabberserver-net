bb5b57253c9f73a462637fe445c60cc5aaa3047a
Nikolay Ivanov <Nikolay.Ivanov@ivanov.avsmedia.net>
2012-08-16 16:18:08 +0000

    bug fix: asynchronous write and ssl

--- trunk/Jabber.Net.Server/Log.cs
+++ trunk/Jabber.Net.Server/Log.cs
@@ -10,14 +10,30 @@ namespace Jabber.Net.Server
             Trace.TraceInformation(message);
         }
 
+        public static void Information(string format, params object[] args)
+        {
+            Trace.TraceInformation(format, args);
+        }
+
         public static void Warning(string message)
         {
             Trace.TraceWarning(message);
+        }
+
+        public static void Warning(string format, params object[] args)
+        {
+            Trace.TraceWarning(format, args);
         }
 
         public static void Error(Exception error)
         {
             Trace.TraceError(error.ToString());
         }
+
+        public static void Error(string format, params object[] args)
+        {
+            Trace.TraceError(format, args);
+        }
+
     }
 }
