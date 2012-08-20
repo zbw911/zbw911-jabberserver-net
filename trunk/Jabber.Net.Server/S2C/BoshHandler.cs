cb0dcdec7f0a33c0e9e3abee1904ebae6db4fb92
John <John@JohnPC>
2012-08-21 00:57:12 +0400

    bosh

---
+++ trunk/Jabber.Net.Server/S2C/BoshHandler.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using agsXMPP.protocol.client;
+using agsXMPP.protocol.extensions.bosh;
+using Jabber.Net.Server.Connections;
+using Jabber.Net.Server.Handlers;
+using Jabber.Net.Server.Sessions;
+
+namespace Jabber.Net.Server.S2C
+{
+    class BoshHandler : XmppHandler, IXmppHandler<Body>
+    {
+        public XmppHandlerResult ProcessElement(Body element, XmppSession session, XmppHandlerContext context)
+        {
+            if (string.IsNullOrEmpty(element.Sid))
+            {
+                return StartBoshSession(element, session, context);
+            }
+            else
+            {
+                session = context.Sessions.GetSession(element.Sid);
+                ((BoshXmppAggregator)session.Connection).AddConnection(element.Rid, session.Connection);
+
+                var body = new Body { };
+            }
+
+            return Void();
+        }
+
+
+        private XmppHandlerResult StartBoshSession(Body element, XmppSession session, XmppHandlerContext context)
+        {
+            var stream = new Stream
+            {
+                Prefix = agsXMPP.Uri.PREFIX,
+                DefaultNamespace = agsXMPP.Uri.CLIENT,
+                Version = element.XmppVersion,
+                To = element.To,
+                From = element.From,
+                Language = element.GetAttribute("xml:lang"),
+            };
+
+            element.Sid = session.Id;
+            element.From = element.To;
+            element.To = null;
+
+            var aggregator = new BoshXmppAggregator(
+                session.Id,
+                TimeSpan.FromSeconds(element.Wait),
+                TimeSpan.FromSeconds(element.Inactivity),
+                TimeSpan.FromMilliseconds(100))
+                .AddConnection(element.Rid, session.Connection);
+            session = new XmppSession(aggregator);
+
+            return Component(Send(session, element), Process(session, stream), Send(session, new BodyEnd()));
+        }
+    }
+}
---
+++ trunk/Jabber.Net.Server/Sessions/XmppSessionManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using agsXMPP;
+using Jabber.Net.Server.Collections;
+
+namespace Jabber.Net.Server.Sessions
+{
+    public class XmppSessionManager
+    {
+        private readonly ReaderWriterLockDictionary<string, XmppSession> sessions = new ReaderWriterLockDictionary<string, XmppSession>(1000);
+
+
+        public XmppSession GetSession(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                return null;
+            }
+
+            XmppSession s;
+            return sessions.TryGetValue(id, out s) ? s : null;
+        }
+
+        public XmppSession GetSession(Jid jid)
+        {
+            Args.NotNull(jid, "jid");
+            return sessions.Values.SingleOrDefault(s => s.Jid == jid);
+        }
+
+        public IEnumerable<XmppSession> GetSessions(Jid jid)
+        {
+            Args.NotNull(jid, "jid");
+            return jid.IsBare ?
+                sessions.Values.Where(s => s.Binded && s.Jid.BareJid == jid.BareJid).ToList() :
+                sessions.Values.Where(s => s.Jid == jid).ToList();
+        }
+
+        public void OpenSession(XmppSession session)
+        {
+            Args.NotNull(session, "session");
+
+            sessions[session.Id] = session;
+        }
+
+        public void CloseSession(string id)
+        {
+            if (!string.IsNullOrEmpty(id))
+            {
+                XmppSession s;
+                sessions.Remove(id, out s);
+                if (s != null)
+                {
+                    s.Connection.Close();
+                }
+            }
+        }
+    }
+}
